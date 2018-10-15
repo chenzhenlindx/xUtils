ebb6b3a11a707e4d719e2e40aee00bf8e9656514
chenzhenlindx <chenzhenlindx@gmail.com>
2018-10-15 13:55:58 +0800

    LiveData+DataBinding

--- app/src/main/java/com/czl/xutils/MainActivity.java
+++ app/src/main/java/com/czl/xutils/MainActivity.java
@@ -1,13 +1,30 @@
 package com.czl.xutils;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.MutableLiveData;
+
 import android.os.Bundle;
 
+import com.blankj.utilcode.util.ToastUtils;
+import com.blankj.utilcode.util.Utils;
+import com.czl.xutils.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    public MutableLiveData<String> name = new MutableLiveData<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Utils.init(getApplicationContext());
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setHandler(this);
+        binding.setLifecycleOwner(this);
+    }
+
+    public void onClick() {
+        String n = name.getValue();
+        ToastUtils.showLong(n);
+        name.setValue("OK");
     }
 }
